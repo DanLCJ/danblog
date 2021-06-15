6e447ff2fc1e660846ae4a85f5675a9b14eaa298
DanLCj <liuchengjia0809@gmail.com>
2021-06-15 22:11:45 +0800

    update 1.2.1 [change dependencies into jsdelivr

--- sw.js
+++ sw.js
@@ -31,8 +31,8 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
+  "blog.atdanliu.cn",
+  "cdn.jsdelivr.net",
   "cdnjs.cloudflare.com"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
